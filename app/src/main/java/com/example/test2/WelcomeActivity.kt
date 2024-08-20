cba9a43768956038b093107ccc9aadd5deb4b0e4
Programeowr <programeowr@gmail.com>
2024-08-20 21:58:32 +0530

    meow

--- app/src/main/java/com/example/test2/WelcomeActivity.kt
+++ app/src/main/java/com/example/test2/WelcomeActivity.kt
@@ -1,22 +1,20 @@
 package com.example.test2
 
-import android.media.Image
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageView
-import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class ComplaintsNearbyActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_complaints_nearby)
+        setContentView(R.layout.activity_welcome)
 
-        val bBtn = findViewById<ImageView>(R.id.pBack)
-        val sBtn = findViewById<Button>(R.id.cSettingsButton)
+        val uBtn = findViewById<Button>(R.id.userButton)
+        val sBtn = findViewById<Button>(R.id.servicesButton)
+
 
 
     }
